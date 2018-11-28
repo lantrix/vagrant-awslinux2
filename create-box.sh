bd2b4a5f32ded9d743dc686bfe0a065697fec217
Lantrix <lantrix@pobox.com>
2018-11-28 16:01:16 +1100

    Better detection

--- create-box.sh
+++ create-box.sh
@@ -1,8 +1,10 @@
-if [[ `uname` -eq 'Darwin' ]]; then
-    hdiutil makehybrid -o seed.iso -hfs -joliet -iso -default-volume-name cidata seedconfig/
-fi
-if [[ `uname` -eq 'Linux' ]]; then
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
     pushd seedconfig/
     genisoimage -output ../seed.iso -volid cidata -joliet -rock user-data meta-data
     popd
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    hdiutil makehybrid -o seed.iso -hfs -joliet -iso -default-volume-name cidata seedconfig/
+else
+    echo "Unknown OS"
+    exit 1
 fi
