57519caddbd3f547c575d189c727095c5b6a9c36
Lantrix <lantrix@pobox.com>
2018-11-30 22:18:45 +1100

    Bit more prep

--- vagrant-prep.sh
+++ vagrant-prep.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-#Delete Bash command history
-export HISTSIZE=0
+
+# Credits to:
+#  - http://vstone.eu/reducing-vagrant-box-size/
+#  - https://github.com/mitchellh/vagrant/issues/343
+
 #Delete cache of yum
 yum clean all
 rm -rf /var/cache/yum
-#Optimize the area of ​​the virtual hard disk
-dd if=/dev/zero of=/ZERO bs=1M
-rm -f /ZERO
+
+# Zero free space to aid VM compression
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# Remove bash history
+export HISTSIZE=0
+rm -f /root/.bash_history
+rm -f /home/ec2-user/.bash_history
+rm -f /home/vagrant/.bash_history
+
+# Cleanup log files
+find /var/log -type f | while read f; do echo -ne '' > $f; done;
